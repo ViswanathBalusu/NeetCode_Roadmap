6d422df69457f7e34523725b4ffaec3d75865ea6
Viswanath <cbalusu@gmu.edu>
2024-04-13 09:54:46 -0500

    Complete Min Stack :+1:

--- Stack/Min_Stack.cs
+++ Stack/Min_Stack.cs
@@ -1,31 +1,60 @@
+//Link: https://leetcode.com/problems/min-stack/
+
+//Min Stack implementation using two stacks
 public class MinStack {
+    Stack<int> stk = null;
+    Stack<int> minStk = null;
 
     public MinStack() {
-        
+        this.stk = new Stack<int>();
+        this.minStk = new Stack<int>();
     }
     
     public void Push(int val) {
-        
+        int prevMin = stk.Count !=0 ? minStk.Peek():int.MaxValue;
+        this.minStk.Push(Math.Min(prevMin, val));
+        this.stk.Push(val);
     }
     
     public void Pop() {
-        
+        this.minStk.Pop();
+        this.stk.Pop();
     }
     
     public int Top() {
-        
+        return this.stk.Peek();
     }
     
     public int GetMin() {
-        
+        return this.minStk.Peek();
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.Push(val);
- * obj.Pop();
- * int param_3 = obj.Top();
- * int param_4 = obj.GetMin();
- */
+
+//Min Stack implementation using a Single List
+public class MinStack {
+    List<int> lst = null;
+
+    public MinStack() {
+        this.lst = new List<int>();
+    }
+    
+    public void Push(int val) {
+        int prevMin = lst.Count !=0 ? lst[lst.Count - 2]:int.MaxValue;
+        this.lst.Add(Math.Min(prevMin, val));
+        this.lst.Add(val);
+    }
+    
+    public void Pop() {
+        this.lst.RemoveAt(lst.Count - 1); // Counts Updated Here
+        this.lst.RemoveAt(lst.Count - 1); // Again remove the last element
+    }
+    
+    public int Top() {
+        return this.lst[lst.Count - 1];
+    }
+    
+    public int GetMin() {
+        return this.lst[lst.Count - 2];
+    }
+}
