1a75eb083f12a10f31458cbfea8f44ac4a5e9912
Viswanath <cbalusu@gmu.edu>
2025-02-02 21:44:06 -0600

    Remove Element

---
+++ AlgorithmsandDataStructuresforBeginners/StaticArrays/RemoveElement.cs
@@ -0,0 +1,22 @@
+﻿// Link: https://leetcode.com/problems/remove-element/submissions/1529292734/
+
+namespace LeetCodeHunt.AlgorithmsandDataStructuresforBeginners.StaticArrays
+{
+    public class SolutionRemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int i = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
+}
